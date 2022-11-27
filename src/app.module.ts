9cbd5bbd25dbbfb0381cffa691489737aa285f66
Vladimir <hotsru12@gmail.com>
2022-11-28 02:28:56 +0300

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,11 @@ import { AuthModule } from './auth/auth.module';
 		}),
 		SequelizeModule.forRoot({
 			dialect: 'postgres',
-			host: process.env.POSTGRES_HOST,
-			port: Number(process.env.POSTGRES_PORT),
-			username: process.env.POSTGRES_USER,
-			password: String(process.env.POSTGRES_PASSWORD),
-			database: process.env.POSTGRES_DB,
+			host: 'localhost',
+			port: 5432,
+			username: 'postgres',
+			password: 'root',
+			database: 'nest-course',
 			models: [User, Role, UserRoles],
 			autoLoadModels: true
 		}),
